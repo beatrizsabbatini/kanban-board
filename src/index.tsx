b6171a56d15280c8a637bd4ab1735e689e8bae1d
Beatriz <beatriz.sabbatini@gmail.com>
2022-06-23 21:28:19 -0300

    feat: added initial redux configuration

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
---
+++ src/store/slices/kanban.slice.ts
@@ -0,0 +1,5 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initiaState = {
+  cards: [],
+}
